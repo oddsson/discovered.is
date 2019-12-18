c8c0ecc6ef5a791fa8a5e653318548331ab5d273
Ívar Oddsson <ivaroddsson@gmail.com>
2019-12-18 22:24:10 +0000

    remove unused code

--- server/utils.js
+++ server/utils.js
@@ -16,45 +16,5 @@ module.exports = {
       grant_type: "client_credentials"
     },
     json: true
-  },
-  newReleasesOptions: {
-    url: "https://api.spotify.com/v1/browse/new-releases?limit=50",
-    headers: {
-      Authorization: `Bearer ${new Buffer(
-        process.env.SPOTIFY_CLIENT_ID + ":" + process.env.SPOTIFY_CLIENT_SECRET
-      ).toString("base64")}`,
-      "Access-Control-Allow-Origin": "*",
-      "Access-Control-Allow-Headers": "X-Requested-With"
-    },
-    json: true
   }
-  // authOptions: {
-  //   url: "https://accounts.spotify.com/api/token",
-  //   headers: {
-  //     Authorization:
-  //       "Basic " +
-  //       new Buffer(
-  //         process.env.SPOTIFY_CLIENT_ID +
-  //           ":" +
-  //           process.env.SPOTIFY_CLIENT_SECRET
-  //       ).toString("base64"),
-  //     "Access-Control-Allow-Origin": "*",
-  //     "Access-Control-Allow-Headers": "X-Requested-With"
-  //   },
-  //   form: {
-  //     grant_type: "client_credentials"
-  //   },
-  //   json: true
-  // },
-  // newReleasesOptions: {
-  //   url: "https://api.spotify.com/v1/browse/new-releases?limit=50",
-  //   headers: {
-  //     Authorization: `Bearer ${new Buffer(
-  //       process.env.SPOTIFY_CLIENT_ID + ":" + process.env.SPOTIFY_CLIENT_SECRET
-  //     ).toString("base64")}`,
-  //     "Access-Control-Allow-Origin": "*",
-  //     "Access-Control-Allow-Headers": "X-Requested-With"
-  //   },
-  //   json: true
-  // }
 };
