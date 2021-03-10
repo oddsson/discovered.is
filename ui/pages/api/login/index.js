f8adda649971ae72dc1e4af6f4c33be2170eb8ca
Ívar Oddsson <ivaroddsson@gmail.com>
2021-03-10 13:30:35 +0000

    Set spotify credentials without env

--- ui/pages/api/login/index.js
+++ ui/pages/api/login/index.js
@@ -7,25 +7,23 @@ const options = {
     Authorization:
       "Basic " +
       new Buffer(
-        process.env.SPOTIFY_CLIENT_ID +
-          ":" +
-          process.env.SPOTIFY_CLIENT_SECRET
+        `${process.env.SPOTIFY_CLIENT_ID}:${process.env.SPOTIFY_CLIENT_SECRET}`
       ).toString("base64"),
     "Access-Control-Allow-Origin": "*",
-    "Access-Control-Allow-Headers": "X-Requested-With"
+    "Access-Control-Allow-Headers": "X-Requested-With",
   },
   form: {
-    grant_type: "client_credentials"
+    grant_type: "client_credentials",
   },
-  json: true
-}
+  json: true,
+};
 
 export default function handle(req, res) {
   try {
-    request.post(options, function(error, response, body) {
+    request.post(options, function (error, response, body) {
       res.end(
         JSON.stringify({
-          token: body.access_token
+          token: body.access_token,
         })
       );
     });
